c9ea429d0e113439de5d66ddc34ea55ff30790cd
ayyappan.g <ayyappang.cs@atlashxm.com>
2024-12-20 17:36:28 +0530

    github actions/docker/grid setup done

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,50 @@
 import pytest
 from selenium.webdriver.chrome.service import Service
-from seleniumwire import webdriver
+#from seleniumwire import webdriver
+from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 
 @pytest.fixture(scope="class", autouse=True, params=["chrome", "edge", "firefox"])
 def setup(request):
     global driver
-    #browser_name = request.config.getoption("--browser_name")
+    # browser_name = request.config.getoption("--browser_name")
     browser_name = request.param
+    remote_url = "http://localhost:4444/wd/hub"
+    #remote_url="http://host.docker.internal:4444/wd/hub"
     if browser_name == "chrome":
-        serviceObj = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=serviceObj)
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        # serviceObj = Service(ChromeDriverManager().install())
+        # driver = webdriver.Chrome(service=serviceObj)
+        driver = webdriver.Remote(command_executor=remote_url, options=chrome_options)
     elif browser_name == "edge":
-        serviceObj = Service(EdgeChromiumDriverManager().install())
-        driver = webdriver.Edge(service=serviceObj)
+        edge_options = EdgeOptions()
+        edge_options.add_argument("--headless")
+        edge_options.add_argument("--disable-gpu")
+        edge_options.add_argument("--no-sandbox")
+        edge_options.add_argument("--disable-dev-shm-usage")
+        # serviceObj = Service(EdgeChromiumDriverManager().install())
+        # driver = webdriver.Edge(service=serviceObj)
+        driver = webdriver.Remote(command_executor=remote_url, options=edge_options)
     elif browser_name == "firefox":
-        serviceObj = Service(GeckoDriverManager().install())
-        driver = webdriver.Firefox(service=serviceObj)
+        firefox_options = FirefoxOptions()
+        firefox_options.add_argument("--headless")
+        firefox_options.add_argument("--disable-gpu")
+        firefox_options.add_argument("--no-sandbox")
+        firefox_options.add_argument("--disable-dev-shm-usage")
+        # serviceObj = Service(GeckoDriverManager().install())
+        # driver = webdriver.Firefox(service=serviceObj)
+        driver = webdriver.Remote(command_executor=remote_url, options=firefox_options)
     driver.maximize_window()
     driver.implicitly_wait(30)
     request.cls.driver = driver
@@ -56,7 +81,7 @@ def pytest_runtest_makereport(item):
         xfail = hasattr(report, 'wasxfail')
         if (report.skipped and xfail) or (report.failed and not xfail):
             filename = report.nodeid.replace("::", "_") + ".png"
-            _capture_screenshot(filename)  # Pass item to capture screenshot
+            # _capture_screenshot(filename)  # Pass item to capture screenshot
             if filename:
                 html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px" onclick="window.open(this.src)" align="right"/></div>' % filename
                 extra.append(pytest_html.extras.html(html))
